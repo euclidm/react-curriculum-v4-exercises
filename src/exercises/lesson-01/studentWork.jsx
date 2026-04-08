69901221ff4bf1405fbe29517c88c9f05d6885b4
yukisoba <marquezeuclidgwyne@gmail.com>
2026-04-07 23:24:31 -0700

    weekly exercise #1
    - added name, age, hobby array
    - returned jsx (about me, map of my hobbies)

--- src/exercises/lesson-01/studentWork.jsx
+++ src/exercises/lesson-01/studentWork.jsx
@@ -2,11 +2,24 @@
 //Exercise: Build an "About Me" Component in this file
 
 export default function StudentWork() {
-  //add variables here
+  // Name, Age, Hobbies
+  const name = "Euclid";
+  const age = "22";
+  const hobbies = ["Playing Videogames", "Tennis", "Going to Coffee Shops", "Taking Photobooth Pictures with Friends"];
+
   return (
     <div>
       {/* add JSX here */}
-      <p> Student output will go here </p>
+      <h1>About {name}</h1>
+      <p>Hi! My name is {name} and I am {age} years old! I am from California, USA. 
+        I recently graduated at UC Irvine with a degree in Game Design & Interactive Media.
+        I love creating interactive and meaningful experiences through game design and UI/UX. 
+        Whether it is designing a game interface, building a menu flow, or crafting graphics for a project, 
+        I enjoy making things that look good and feel good to use!</p>
+
+      <p>A few hobbies I enjoy are:</p>
+
+      <ul>{hobbies.map((hobby, index) => (<li key={index}> {hobby} </li>))}</ul>
     </div>
   );
 }

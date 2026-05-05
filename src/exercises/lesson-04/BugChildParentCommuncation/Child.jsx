93b5bdd5c322143c044728ed068d06e70a240573
yukisoba <marquezeuclidgwyne@gmail.com>
2026-05-04 21:58:47 -0700

    Worked on Week 4 Exercise

--- src/exercises/lesson-04/BugChildParentCommuncation/Child.jsx
+++ src/exercises/lesson-04/BugChildParentCommuncation/Child.jsx
@@ -1,3 +1,3 @@
-export default function Child() {
-  return <button onClick={() => {}}>Increment Counter</button>;
+export default function Child({incrementObj}) {
+  return <button onClick={() => {incrementObj()}}>Increment Counter</button>;
 }
